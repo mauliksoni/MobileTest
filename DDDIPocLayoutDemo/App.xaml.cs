5b825adf6164777fca0af31c2b15d64b129007d2
DPI <dpi@dpis-imac.attlocal.net>
2017-12-28 17:19:26 -0600

    added

---
+++ DDDIPocLayoutDemo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using DLToolkitControlsSamples;
+using DLToolkit.Forms.Controls;
+
+namespace DDDIPocLayoutDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new BookFlowListView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
